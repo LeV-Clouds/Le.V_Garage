9bee5315c8903bd0162e2290cf2df8267c1e4d6c
Le.V <78176893+LeV-Clouds@users.noreply.github.com>
2022-06-21 17:18:59 +0200

    Update d'un commentaire dégueulasse

--- Le.V_Garage/garage_s.lua
+++ Le.V_Garage/garage_s.lua
@@ -59,7 +59,7 @@ ESX.RegisterServerCallback('core:GetOwnedVeh', function(source, callback, Propri
     end)
 end)
 
-RegisterServerEvent("core:RenameVeh") -- Renommer
+RegisterServerEvent("core:RenameVeh") -- T UN FDP
 AddEventHandler("core:RenameVeh", function(Plaque, Nom)
     MySQL.Async.execute("UPDATE player_vehicles SET nom = @nom WHERE plaque = @plaque",{
         ['@nom'] = Nom,
